c986348d8dc656fe8f03f1e8e0a547e1416d9997
Константин Лапчевский <l-sky.k@yandex.ua>
2016-02-28 00:57:31 +0200

    style

--- composePage.R
+++ composePage.R
@@ -11,13 +11,12 @@ OSSComposePage <- function(type="rds"){
     page <- append(page,"<!DOCTYPE html>")
     page <- append(page,"<html>")
     page <- append(page,"<head>")
-    page <- append(page,"<meta charset=\"windows-1251\">")
+    page <- append(page,"<meta charset=\"utf-8\">")
     page <- append(page,"<title>онкнфеммъ опн ярсдемряэйе яюлнбпъдсбюммъ с йх╞бяэйнлс мюж╡нмюкэмнлс см╡бепяхрер╡ ╡лем╡ рюпюяю ьебвемйю</title>")
     page <- append(page,"</head>")
     page <- append(page,"<body>")
     page <- append(page,"<h2 style=\"text-align:center\">онкнфеммъ опн ярсдемряэйе яюлнбпъдсбюммъ с йх╞бяэйнлс мюж╡нмюкэмнлс см╡бепяхрер╡ ╡лем╡ рюпюяю ьебвемйю</h2><br>")
     page <- append(page,"<h2 style=\"text-align:center; margin-bottom:0px\">гЛЁЯР</h2><br>")
-    
     for(i in 1:dim(oss_frame)[1]){
         subpart <- oss_frame$subpart[i]
         part <- oss_frame$part[i]
@@ -33,25 +32,27 @@ OSSComposePage <- function(type="rds"){
         }
     }
     
+    page <- append(page,"<div style=\"width:800px\">")
+    
     for(i in 1:dim(oss_frame)[1]){
         subpart <- oss_frame$subpart[i]
         part <- oss_frame$part[i]
         act <- oss_frame$act[i]
         chapter <- oss_frame$chapter[i]
         text <- oss_frame$text[i]
-        
         if(subpart!=0){
             page <- append(page,paste0("<div class=\"subpart\" style=\"margin-left:40px\">",subpart,") ",text,"</div><br>",collapse=""))
         } else if(part!=0){
             page <- append(page,paste0("<div class=\"part\">",chapter,".",act,".",part,". ",text,"</div><br>",collapse=""))
         } else if(act!=0){
-            page <- append(page,paste0("<div class=\"act\" id=\"",chapter,".",act,"\">","<b>цКЮБЮ ",chapter,".",act,". ",text,"</b></div><hr>",collapse=""))
+            page <- append(page,paste0("<div class=\"act\" style=\"font-size:20px\" id=\"",chapter,".",act,"\">","<b>цКЮБЮ ",chapter,".",act,". ",text,"</b></div><hr>",collapse=""))
         } else if(chapter!=0){
-            page <- append(page,paste0("<h2 class=\"chapter\" id=\"",chapter,"\">","пНГДЁК ",chapter,". ",text,"</h2><br>",collapse=""))
+            page <- append(page,paste0("<h2 class=\"chapter\" style=\"text-align:center;margin-bottom:0px;\" id=\"",chapter,"\">","пНГДЁК ",chapter,". ",text,"</h2><br>",collapse=""))
         }
     }
+    page <- append(page,"</div>")
     page <- append(page,"</body>")
     page <- append(page,"</html>")
-    writeLines(page,"index.html")
+    writeLines(iconv(page,from = "windows-1251",to="utf-8"),"index.html",useBytes = TRUE)
     return(page)
 }
